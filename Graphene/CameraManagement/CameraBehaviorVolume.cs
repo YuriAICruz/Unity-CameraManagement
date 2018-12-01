8659b0be95c772923293dff6b744792839b3962d
Yuri Cruz <yuriaicruz@gmail.com>
2018-12-01 10:53:46 -0200

    init

---
+++ Graphene/CameraManagement/CameraBehaviorVolume.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Graphene.CameraManagement
+{
+    public class CameraBehaviorVolume : MonoBehaviour
+    {
+        public float Radius;
+
+        public Vector3 CamraOffset;
+        public Vector3 CamraEulerAngles;
+
+        public bool Fixed;
+    }
+}
